48f1f84916759e300e2177828d49b9f178d83c26
ramankarki <ramankarki40@gmail.com>
2020-12-16 22:38:37 +0545

    with Logo, darkThemeSwitchBtn and Nav component combined made responsive header with light and dark theme switching affect

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./css/Header.css";
+import Logo from "../Logo/Logo";
+import DarkThemeSwitchBtn from "../Dark-Theme-Switch-Btn/DarkThemeSwitchBtn";
+import Nav from "../Nav/Nav";
+
+function Header(props) {
+  let headerBg;
+  if (props.isdarkThemeActive) {
+    headerBg = "header-wrapper header-wrapper-active";
+  } else {
+    headerBg = "header-wrapper";
+  }
+
+  return (
+    <div className={headerBg}>
+      <header className="header container">
+        <Logo dark={props.isdarkThemeActive} />
+        <DarkThemeSwitchBtn
+          dark={props.isdarkThemeActive}
+          changeTheme={props.switchActiveTheme}
+        />
+        <Nav dark={props.isdarkThemeActive} />
+      </header>
+    </div>
+  );
+}
+
+export default Header;
