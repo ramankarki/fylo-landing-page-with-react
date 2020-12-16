808705eae8fe9a0346fe00981cbb7dcef98ecb4d
ramankarki <ramankarki40@gmail.com>
2020-12-16 15:24:24 +0545

    initialized project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FeatureCard from "./Feature-Card/FeatureCard";
+
+function App() {
+  return (
+    <header>
+      This is a header
+      <FeatureCard />
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/Feature-Card/FeatureCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FeatureCard.css";
+
+let FeatureCard = () => {
+  return (
+    <div>
+      <h1>This is a feature card</h1>
+    </div>
+  );
+};
+
+export default FeatureCard;
