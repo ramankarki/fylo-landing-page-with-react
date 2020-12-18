e1aa8805fea1c5b5aa2791ce7f8d2b36edd966bf
ramankarki <ramankarki40@gmail.com>
2020-12-18 15:11:32 +0545

    responsive hero section with dark switch complete

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./css/Hero.css";
+import Picture from "../Picture/Picture";
+import Article from "../Article/Article";
+import Form from "../Form/Form";
+
+export default function Hero(props) {
+  return (
+    <div
+      className={
+        props.isdarkThemeActive
+          ? "Hero-wrapper Hero-wrapper-dark"
+          : "Hero-wrapper"
+      }
+    >
+      <article className="Hero">
+        <Picture
+          isdarkThemeActive={props.isdarkThemeActive}
+          imgName="files-store-illustration.svg"
+        />
+        <div>
+          <Article
+            headingType="heading"
+            isdarkThemeActive={props.isdarkThemeActive}
+            heading="All your files in one secure location, accessible anywhere"
+            paragraph={[
+              "Flyo stores your important files in one secure location. Access them wherever you need, share and collaborate with friends, family and co-workers.",
+            ]}
+          />
+          <Form buttonValue="Get started" hero={true} />
+        </div>
+      </article>
+    </div>
+  );
+}
