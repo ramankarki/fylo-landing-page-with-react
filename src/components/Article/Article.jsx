c84c8abef1b312611a1cd7cc3fb5fc546a6d3a21
ramankarki <ramankarki40@gmail.com>
2020-12-18 15:12:37 +0545

    added conditional rendering for heading and sub-heading, normal p and centered p

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,16 +4,33 @@ import "./css/Article.css";
 export default function Article(props) {
   function renderParagraph(p, index) {
     return (
-      <p className="paragraph" key={index}>
+      <p
+        className={
+          props.headingType === "sub-heading"
+            ? "paragraph paragraph-center"
+            : "paragraph"
+        }
+        key={index}
+      >
         {p}
       </p>
     );
   }
 
   return (
-    <div className="article-wrapper article-wrapper-ml-lg">
+    <div
+      className={
+        props.headingType === "sub-heading"
+          ? "article-wrapper"
+          : "article-wrapper article-wrapper-ml-lg"
+      }
+    >
       <h1
-        className={props.isdarkThemeActive ? "heading heading-dark" : "heading"}
+        className={
+          props.isdarkThemeActive
+            ? `${props.headingType} ${props.headingType}-dark`
+            : `${props.headingType}`
+        }
       >
         {props.heading}
       </h1>
