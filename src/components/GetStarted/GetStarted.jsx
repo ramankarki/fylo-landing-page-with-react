e29a59562a276c867622b44289a7c992caeb2685
ramankarki <ramankarki40@gmail.com>
2020-12-19 17:37:07 +0545

    added wrapper for GetStarted to fix components gaps

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -6,27 +6,35 @@ export default function GetStarted(props) {
   return (
     <div
       className={
-        props.isdarkThemeActive ? "GetStarted GetStarted-dark" : "GetStarted"
+        props.isdarkThemeActive
+          ? "GetStarted-wrapper GetStarted-wrapper-dark"
+          : "GetStarted-wrapper"
       }
     >
-      <h2
+      <div
         className={
-          props.isdarkThemeActive
-            ? "GetStarted-heading GetStarted-heading-dark"
-            : "GetStarted-heading"
+          props.isdarkThemeActive ? "GetStarted GetStarted-dark" : "GetStarted"
         }
       >
-        Get early access today
-      </h2>
-      <p className="GetStarted-text">
-        It only takes a minute to sign up and our free starter tier is extremely
-        generous. If you have any questions, our support team would be happy to
-        help you.
-      </p>
-      <Form
-        buttonValue="Get started for free"
-        isdarkThemeActive={props.isdarkThemeActive}
-      />
+        <h2
+          className={
+            props.isdarkThemeActive
+              ? "GetStarted-heading GetStarted-heading-dark"
+              : "GetStarted-heading"
+          }
+        >
+          Get early access today
+        </h2>
+        <p className="GetStarted-text">
+          It only takes a minute to sign up and our free starter tier is
+          extremely generous. If you have any questions, our support team would
+          be happy to help you.
+        </p>
+        <Form
+          buttonValue="Get started for free"
+          isdarkThemeActive={props.isdarkThemeActive}
+        />
+      </div>
     </div>
   );
 }
